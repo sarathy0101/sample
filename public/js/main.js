9f60ca335cba70153122b225469646d249659780
sarathy <sarathy0101@gmail.com>
2021-04-25 22:21:40 +0530

    after making chatbox scrollable

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,24 @@
 const chatForm=document.getElementById('chat-form')
+const chatMessage=document.querySelector('#chat-messages')
 let socket=io()
 socket.on('message',(message)=>
 {
-    console.log(message)
+    
     displayMessage(message)
+    chatMessage.scrollTop=chatMessage.scrollHeight
 })
 chatForm.addEventListener('submit',(e)=>
 {
     e.preventDefault();
     const msg=chatBox.chat.value
     socket.emit('chatMessage',msg)
+    chatBox.chat.value=''
 })
 function displayMessage(message)
 {
     const divForMessage=document.createElement('div')
     divForMessage.classList.add('message')
-    divForMessage.innerHTML=`<p class="chat-content">${message}</p>`
+    divForMessage.innerHTML=`<p class="chat-content" style="word-wrap:break-word">${message}</p>`
     document.querySelector('#chat-messages').appendChild(divForMessage)
 
 }
