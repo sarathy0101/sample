3e7acfac6849d9d0eb8103883987dc94c4dec32c
sarathy <sarathy0101@gmail.com>
2021-04-26 22:49:40 +0530

    after time format try1

--- utils/messages.js
+++ utils/messages.js
@@ -1,8 +1,17 @@
 const moment=require('moment')
 function formatMessage(userName,messageContent)
 {
+    let date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
     return {
-        userName,messageContent,time:moment().format('h:mm a')
+        userName,messageContent,time:strTime
     }
 }
 
